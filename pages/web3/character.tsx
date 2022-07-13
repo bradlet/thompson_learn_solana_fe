85af7753ebc3f7e8dafcc7d385aff0fa39e89ac4
Bradley Thompson <bradlet2@pdx.edu>
2022-07-13 08:41:37 -0700

    Use getServerSideProps for test data

--- pages/web3/character.tsx
+++ pages/web3/character.tsx
@@ -1,13 +1,34 @@
 import { NextPage } from "next";
+import Link from "next/link";
 import TopBar from "../../components/TopBar";
+// import * as solanaW3 from '@solana/web3.js';
+// console.log(solanaW3);
 
-const Character: NextPage = () => {
+export async function getServerSideProps(context: any) {
+    const test = {
+        color: "blue"
+    }
+
+    return {
+        props: {
+            data: test
+        }
+    }
+}
+
+const Character: NextPage = ({ data }: any) => {
   return (
     <div>
       <TopBar pageTitle="Character sheet" />
-      <main></main>
+      <main>
+          {data.color}
+      </main>
+      <Link href="/">
+        <a>Return to home page</a>
+      </Link>
     </div>
   );
 };
+
 
 export default Character;
